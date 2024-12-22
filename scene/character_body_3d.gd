940aaf29c95e59dbeaf610b7655ddccc050a45eb
GuitarDog <Colinpanarra@gmail.com>
2024-12-22 17:26:55 -0500

    rotating

--- scene/character_body_3d.gd
+++ scene/character_body_3d.gd
@@ -2,16 +2,15 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+#onst gravity = -9.8
 
 func _physics_process(delta: float) -> void:
 
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.x)).normalized()
+	var input_dir := Input.get_vector("move_left", "move_right", "move_down", "move_up")
+	var direction := Vector3(input_dir.x, input_dir.y, 0).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.y = direction.y * SPEED
@@ -19,4 +18,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
+	rotate_z(.1)
 	move_and_slide()
